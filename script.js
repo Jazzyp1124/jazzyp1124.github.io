fc3e1796709f1b0d3ca51319c53de489eba614ba
Jasmine Plummer <50575614+Jazzyp1124@users.noreply.github.com>
2024-11-19 23:26:29 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+<script src="script.js"></script>
+document.querySelectorAll('a[href^="#portfolio"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
